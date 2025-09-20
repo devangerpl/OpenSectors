4f4ff0e186e434e9bb263bef2e9be6b1f020ca09
Fizu <kacpereklubixx@gmail.com>
2025-09-20 20:38:16 +0200

    code improvement

--- bridge/src/main/java/net/lightcode/bridge/listener/PlayerServerConnectListener.java
+++ bridge/src/main/java/net/lightcode/bridge/listener/PlayerServerConnectListener.java
@@ -1,12 +1,16 @@
 package net.lightcode.bridge.listener;
 
 import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.connection.PostLoginEvent;
+import com.velocitypowered.api.event.connection.PreLoginEvent;
 import com.velocitypowered.api.event.player.ServerPreConnectEvent;
 import com.velocitypowered.api.proxy.Player;
 import net.kyori.adventure.text.Component;
 import net.lightcode.bridge.BridgePlugin;
 import net.lightcode.sector.Sector;
 import net.lightcode.sector.type.SectorType;
+
+import java.util.UUID;
 
 public class PlayerServerConnectListener {
 
@@ -20,18 +24,26 @@ public class PlayerServerConnectListener {
     public void onServerPreConnect(ServerPreConnectEvent event) {
         final Player player = event.getPlayer();
 
-        String lastSector = this.plugin.networkService().databaseConnection().sync().get(player.getUniqueId().toString());
+        String lastSector = this.plugin.networkService().databaseConnection()
+                .sync()
+                .get(player.getUniqueId().toString());
 
         Sector sector = this.plugin.sectorService().find(SectorType.SPAWN);
 
-        if(lastSector != null) {
-            sector = this.plugin.sectorService().find(lastSector);
+        if (lastSector != null) {
+            Sector foundSector = this.plugin.sectorService().find(lastSector);
+
+            if (foundSector != null) {
+                sector = foundSector;
+            }
+
         }
 
-        this.plugin.networkService().databaseConnection().sync().set(player.getUniqueId().toString(), sector.id());
+        this.plugin.networkService().databaseConnection()
+                .sync()
+                .set(player.getUniqueId().toString(), sector.id());
 
-        this.plugin.server().getServer(sector.id()).ifPresentOrElse(server ->
-                        event.setResult(ServerPreConnectEvent.ServerResult.allowed(server)),
+        this.plugin.server().getServer(sector.id()).ifPresentOrElse(server -> event.setResult(ServerPreConnectEvent.ServerResult.allowed(server)),
                 () -> {
                     player.disconnect(Component.text("Brak dostępnych serwerów."));
                     event.setResult(ServerPreConnectEvent.ServerResult.denied());
